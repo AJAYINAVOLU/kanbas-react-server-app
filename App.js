8b7e1235fef5d17388f20eb271a5ac9f3a23fc6d
AJAY <inavolu.a@northeastern.edu>
2024-04-23 22:13:47 -0400

    Minor Bugs_V3_test

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: 'https://a6--zingy-starship-af0caa.netlify.app',    // Restrict cross origin resource sharing to the react application.
+        origin: process.env.FRONTEND_URL,    // Restrict cross origin resource sharing to the react application.
         credentials: true,                   // Support cookies.
     })
 );
