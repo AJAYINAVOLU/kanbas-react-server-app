d4647819ab35ed425a316138b0a557e6353063b0
AJAY <inavolu.a@northeastern.edu>
2024-04-23 21:55:29 -0400

    Minor Bugs_V3_test

--- App.js
+++ App.js
@@ -36,11 +36,10 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false
 };
-console.log("IN APP.JS. process.env.NODE_ENV = " + process.env.NODE_ENV);
 
 if (process.env.NODE_ENV !== "development") {
-    // console.log("IN APP.JS. process.env.NODE_ENV = " + process.env.NODE_ENV);
-    // sessionOptions.proxy = true;
+    console.log("IN APP.JS. process.env.NODE_ENV = " + process.env.NODE_ENV);
+    sessionOptions.proxy = true;
     // sessionOptions.cookie = {
     //     sameSite: "none",
     //     secure: true
